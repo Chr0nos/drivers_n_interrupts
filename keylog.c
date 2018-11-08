dd66d512c3d34256fd9d65fcff9625c2bef40be8
Chr0nos <snicolet@student.42.fr>
2018-11-08 12:55:57 +0100

    fix

--- keylog.c
+++ keylog.c
@@ -219,10 +219,8 @@ static void		bonus_iterate(struct key_log_entry *log, void *ptr)
 		return;
 	ascii = (log->upper_case) ? log->key->ascii_up : log->key->ascii;
 	if (ascii == '\b') {
-		if (seq->count > 0) {
+		if (seq->count > 0)
 			seq->count -= 1;
-			seq->index -= 1;
-		}
 	} else if (isprint(ascii) || ascii == '\n')
 		seq_putc(seq, ascii);
 }
